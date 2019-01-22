8c21b2c0f3d674ce3617eb8546d106d082240762
T FLock <thomas@gradumante.com>
2019-01-22 16:26:12 -0500

    pushing to work form home

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-        
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+        {/* can add javascript functions right inline of the html */}
+          Copyright &copy;{new Date().getFullYear()} DevConnector
+        </footer>
     </div>
   )
 }
