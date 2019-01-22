c3037efa64539cba26d9897aca90c38810e2a543
T FLock <thomas@gradumante.com>
2019-01-22 16:08:41 -0500

    push for home work

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar />
+        <h1>My react app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
